093dad4fae1e407f2e544c7abd7dca68c57efba8
tadashi1104 <1tadashi12s9@gmail.com>
2019-07-13 14:22:48 +0900

    整えた

---
+++ save-state/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using save_state.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace save_state
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CalendarView();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
