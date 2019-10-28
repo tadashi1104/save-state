03fd55f6a2263cee551c7f46707b14c80fba41da
tadashi1104 <1tadashi12s9@gmail.com>
2019-10-29 07:31:43 +0900

    OXYPlot初期化処理追記

--- save-state.iOS/AppDelegate.cs
+++ save-state.iOS/AppDelegate.cs
@@ -24,6 +24,7 @@ namespace save_state.iOS
         {
             Xamarin.Calabash.Start();
             global::Xamarin.Forms.Forms.Init();
+            OxyPlot.Xamarin.Forms.Platform.iOS.PlotViewRenderer.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
