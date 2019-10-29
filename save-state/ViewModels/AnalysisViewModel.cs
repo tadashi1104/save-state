b2402062de9f0f84f89aebccc883e0ad3649ce48
tadashi1104 <1tadashi12s9@gmail.com>
2019-10-30 08:06:52 +0900

    棒グラフ、折れ線グラフ描画

--- save-state/ViewModels/AnalysisViewModel.cs
+++ save-state/ViewModels/AnalysisViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using OxyPlot;
+using OxyPlot.Axes;
 using OxyPlot.Series;
 
 namespace save_state.ViewModels
@@ -7,19 +8,80 @@ namespace save_state.ViewModels
 
     public class AnalysisViewModel
     {
+
+        public PlotModel plotModelGood { get; }
         public PlotModel plotModel { get; }
 
         public AnalysisViewModel()
         {
-            this.plotModel = new PlotModel { Title = "Hello OxyPlot" };
-            this.plotModel.Series.Add(new LineSeries
+            this.plotModelGood = new PlotModel { Title = "A" };
+
+            var xaxis = new CategoryAxis();
+            xaxis.Position = AxisPosition.Bottom;
+            xaxis.MajorGridlineStyle = LineStyle.Solid;
+            xaxis.MinorGridlineStyle = LineStyle.Dot;
+            xaxis.Labels.Add("10/06");
+            xaxis.Labels.Add("10/07");
+            xaxis.Labels.Add("10/08");
+            xaxis.Labels.Add("10/09");
+            xaxis.Labels.Add("10/10");
+            xaxis.Labels.Add("10/11");
+            xaxis.Labels.Add("10/12");
+            xaxis.FontSize = 8.0;
+
+            var yaxis = new LinearAxis();
+            yaxis.Position = AxisPosition.Left;
+            yaxis.MajorGridlineStyle = LineStyle.Dot;
+            yaxis.MinorGridlineStyle = LineStyle.Dot;
+
+            var columnSeriesGood = new ColumnSeries();
+            columnSeriesGood.Items.Add(new ColumnItem(5));
+            columnSeriesGood.Items.Add(new ColumnItem(8));
+            columnSeriesGood.Items.Add(new ColumnItem(2));
+            columnSeriesGood.Items.Add(new ColumnItem(12));
+            columnSeriesGood.Items.Add(new ColumnItem(4));
+            columnSeriesGood.Items.Add(new ColumnItem(1));
+            columnSeriesGood.Items.Add(new ColumnItem(9));
+
+            plotModelGood.Axes.Add(xaxis);
+            plotModelGood.Axes.Add(yaxis);
+            plotModelGood.Series.Add(columnSeriesGood);
+
+
+            plotModel = new PlotModel { Title = "B" };
+
+
+            var xaxisLine = new CategoryAxis();
+            xaxisLine.Position = AxisPosition.Bottom;
+            xaxisLine.MajorGridlineStyle = LineStyle.Solid;
+            xaxisLine.MinorGridlineStyle = LineStyle.Dot;
+            xaxisLine.Labels.Add("10/06");
+            xaxisLine.Labels.Add("10/07");
+            xaxisLine.Labels.Add("10/08");
+            xaxisLine.Labels.Add("10/09");
+            xaxisLine.Labels.Add("10/10");
+            xaxisLine.Labels.Add("10/11");
+            xaxisLine.Labels.Add("10/12");
+            xaxisLine.FontSize = 8.0;
+
+            var yaxisLine = new LinearAxis();
+            yaxisLine.Position = AxisPosition.Left;
+            yaxisLine.MajorGridlineStyle = LineStyle.Dot;
+            yaxisLine.MinorGridlineStyle = LineStyle.Dot;
+
+            plotModel.Axes.Add(xaxisLine);
+            plotModel.Axes.Add(yaxisLine);
+            plotModel.Series.Add(new LineSeries
             {
                 Points =
                 {
-                    new DataPoint(0, 10),
-                    new DataPoint(1, 20),
-                    new DataPoint(2, 15),
-                    new DataPoint(3, 40),
+                    new DataPoint(0, 2.5),
+                    new DataPoint(1, 3.0),
+                    new DataPoint(2, 4.8),
+                    new DataPoint(3, 2.2),
+                    new DataPoint(4, 3.4),
+                    new DataPoint(5, 5.0),
+                    new DataPoint(6, 1.2),
                 }
             });
         }
