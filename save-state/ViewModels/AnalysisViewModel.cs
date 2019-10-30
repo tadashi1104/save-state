bd0b7a3490427e91cb3e49b7b89101598970f3ca
tadashi1104 <1tadashi12s9@gmail.com>
2019-10-31 08:15:27 +0900

    複合グラフ追加

--- save-state/ViewModels/AnalysisViewModel.cs
+++ save-state/ViewModels/AnalysisViewModel.cs
@@ -11,10 +11,11 @@ namespace save_state.ViewModels
 
         public PlotModel plotModelGood { get; }
         public PlotModel plotModel { get; }
+        public PlotModel plotModelComposite { get; }
 
         public AnalysisViewModel()
         {
-            this.plotModelGood = new PlotModel { Title = "A" };
+            this.plotModelGood = new PlotModel { Title = "A" }; //良かったこと
 
             var xaxis = new CategoryAxis();
             xaxis.Position = AxisPosition.Bottom;
@@ -33,6 +34,7 @@ namespace save_state.ViewModels
             yaxis.Position = AxisPosition.Left;
             yaxis.MajorGridlineStyle = LineStyle.Dot;
             yaxis.MinorGridlineStyle = LineStyle.Dot;
+            yaxis.FontSize = 8.0;
 
             var columnSeriesGood = new ColumnSeries();
             columnSeriesGood.Items.Add(new ColumnItem(5));
@@ -47,9 +49,7 @@ namespace save_state.ViewModels
             plotModelGood.Axes.Add(yaxis);
             plotModelGood.Series.Add(columnSeriesGood);
 
-
-            plotModel = new PlotModel { Title = "B" };
-
+            plotModel = new PlotModel { Title = "B" }; //気分
 
             var xaxisLine = new CategoryAxis();
             xaxisLine.Position = AxisPosition.Bottom;
@@ -68,6 +68,7 @@ namespace save_state.ViewModels
             yaxisLine.Position = AxisPosition.Left;
             yaxisLine.MajorGridlineStyle = LineStyle.Dot;
             yaxisLine.MinorGridlineStyle = LineStyle.Dot;
+            yaxisLine.FontSize = 8.0;
 
             plotModel.Axes.Add(xaxisLine);
             plotModel.Axes.Add(yaxisLine);
@@ -84,6 +85,64 @@ namespace save_state.ViewModels
                     new DataPoint(6, 1.2),
                 }
             });
+
+            plotModelComposite = new PlotModel { Title = "C" }; // 複合
+
+            var xaxisComposite = new CategoryAxis();
+            xaxisComposite.Position = AxisPosition.Bottom;
+            xaxisComposite.MajorGridlineStyle = LineStyle.Solid;
+            xaxisComposite.MinorGridlineStyle = LineStyle.Dot;
+            xaxisComposite.Labels.Add("10/06");
+            xaxisComposite.Labels.Add("10/07");
+            xaxisComposite.Labels.Add("10/08");
+            xaxisComposite.Labels.Add("10/09");
+            xaxisComposite.Labels.Add("10/10");
+            xaxisComposite.Labels.Add("10/11");
+            xaxisComposite.Labels.Add("10/12");
+            xaxisComposite.FontSize = 8.0;
+
+            var yaxisComposite1 = new LinearAxis();
+            yaxisComposite1.Position = AxisPosition.Left;
+            yaxisComposite1.MajorGridlineStyle = LineStyle.Dot;
+            yaxisComposite1.MinorGridlineStyle = LineStyle.Dot;
+            yaxisComposite1.FontSize = 8.0;
+
+            var yaxisComposite2 = new LinearAxis();
+            yaxisComposite2.Position = AxisPosition.Right;
+            yaxisComposite2.Maximum = 5;
+            yaxisComposite2.Minimum = 0;
+            yaxisComposite2.MajorGridlineStyle = LineStyle.Dot;
+            yaxisComposite2.MinorGridlineStyle = LineStyle.Dot;
+            yaxisComposite2.FontSize = 8.0;
+
+            var columnSeriesComposite = new ColumnSeries();
+            columnSeriesComposite.Items.Add(new ColumnItem(5));
+            columnSeriesComposite.Items.Add(new ColumnItem(8));
+            columnSeriesComposite.Items.Add(new ColumnItem(2));
+            columnSeriesComposite.Items.Add(new ColumnItem(12));
+            columnSeriesComposite.Items.Add(new ColumnItem(4));
+            columnSeriesComposite.Items.Add(new ColumnItem(1));
+            columnSeriesComposite.Items.Add(new ColumnItem(9));
+
+            plotModelComposite.Axes.Add(xaxisComposite);
+            plotModelComposite.Axes.Add(yaxisComposite1);
+            plotModelComposite.Axes.Add(yaxisComposite2);
+            plotModelComposite.Series.Add(columnSeriesComposite);
+            plotModelComposite.Series.Add(new LineSeries
+            {
+                Points =
+                {
+                    new DataPoint(0, 2.5),
+                    new DataPoint(1, 3.0),
+                    new DataPoint(2, 4.8),
+                    new DataPoint(3, 2.2),
+                    new DataPoint(4, 3.4),
+                    new DataPoint(5, 5.0),
+                    new DataPoint(6, 1.2),
+                }
+            });
+
+
         }
     }
 }
