380dcdf428282410ee806bdb5417137c8bf53e93
tadashi1104 <1tadashi12s9@gmail.com>
2019-11-05 08:00:15 +0900

    MasterDetailPage追加

---
+++ save-state/Views/MasterDetailPageMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace save_state.Views
+{
+    public class MasterDetailPageMenuItem
+    {
+        public MasterDetailPageMenuItem()
+        {
+            TargetType = typeof(MasterDetailPageDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
